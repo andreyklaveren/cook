5a494808bc2907095ab8aa95868f587e067b1c42
andreyklaveren <you@example.comandreyklaveren@gmail.com>
2024-03-06 23:05:24 -0300

    alt.

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,7 +1,11 @@
+import * as recipes from "./recipesService"
+import * as preparations from "./preparationsService"
 import * as ingredients from "./ingredientsService"
 
 export const services = {
+    recipes,
     ingredients,
+    preparations,
 
     storage: {
         imagePath: 
